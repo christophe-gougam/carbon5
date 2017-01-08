00171590358af441b429ae2410ae8fa88a6b2e89
Christophe G <Christophe G@PC-Chris>
2017-01-08 18:19:53 +0100

    Ajout de la méthode supprimerEnBase()

--- carbon5/src/r0/CarManager.java
+++ carbon5/src/r0/CarManager.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 public class CarManager {
 
 	public static void main(String[] args) {
+		supprimerEnBase("Audi");
 		sauverEnBase("Audi");
 		lireEnBase();
 	}
@@ -103,4 +104,47 @@ public class CarManager {
 			}
 		}
 	}
+	
+	public static void supprimerEnBase(String marque) {
+
+		// Information d'accès à la base de données
+		String url = "jdbc:mysql://localhost:3306/carbon5";
+		String login = "root";
+		String passwd = "";
+		Connection cn =null;
+		Statement st =null;
+
+		try {
+
+			// Etape 1 : Chargement du driver
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// Etape 2 : récupération de la connexion
+			cn = DriverManager.getConnection(url, login, passwd);
+
+			// Etape 3 : Création d'un statement
+			st = cn.createStatement();
+
+			String sql = "DELETE FROM `car` WHERE `marque`=('" + marque + "')";
+
+			// Etape 4 : exécution requête
+			st.executeUpdate(sql);
+
+			// Si récup données alors étapes 5 (parcours Resultset)
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			try {
+			// Etape 6 : libérer ressources de la mémoire.
+				cn.close();
+				st.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+}
