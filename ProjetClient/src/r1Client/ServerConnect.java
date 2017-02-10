4a97a05893b708415e50b5745df843c0126516a1
pierre.hauser <pierre.hauser@10.10.77.2>
2017-02-10 19:30:00 +0100

    Add properties file for server address and port in Client side

--- ProjetClient/src/r1Client/ServerConnect.java
+++ ProjetClient/src/r1Client/ServerConnect.java
@@ -1,8 +1,13 @@
 package r1Client;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.*;
 
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Properties;
+
+import r1Serveur.ConnectionPool;
 
 public class ServerConnect implements Runnable{
 	
@@ -11,7 +16,7 @@ public class ServerConnect implements Runnable{
 	public static int portServer = 50000;
 	public ArrayList<String> data;
 	public String identifier;
-	public String serverAddress = "127.0.0.1";
+	public String serverAddress = "10.10.10.210";
 	
 	public ServerConnect(ArrayList<String> data, String identifier) {
 	
@@ -22,6 +27,28 @@ public class ServerConnect implements Runnable{
 	
 	public void run(){
 		try{
+			
+			Properties prop = new Properties();
+			String portServerFile;
+			String serverAddressFile;
+			InputStream input = null;
+			String filename = "configClient.properties" ;
+			
+			input = ServerConnect.class.getClassLoader().getResourceAsStream(filename);
+			
+			if (input == null) {
+				System.out.println("Sorry, unable to find " + filename);
+			}
+			// load a properties file
+			try {
+				prop.load(input);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			portServer = Integer.parseInt(prop.getProperty("portServer"));
+			serverAddress = prop.getProperty("serverAddress");
 			//String serverAdress = InetAddress.getLocalHost().getHostAddress().toString();
 			System.out.println("Ouverture de la socket avec l'adresse/port du serveur et tentative de connexion");
 			socket = new Socket(serverAddress, portServer);
