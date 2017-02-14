48d380e5c94e1a1c0970a31385a7d3583884784a
Thierno6Diallo <thierno6.diallo@etu.u-pec.fr>
2017-02-15 00:51:06 +0100

    RM de carbon.sql

---
+++ ProjetClient/src/r1Client/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package r1Client;
+
+/**
+ *
+ * @author CongThuan
+ */
+public class Test {
+	
+    public static void main(String[] args) {
+    	Fenetre fenetre=new Fenetre();
+    	fenetre.setVisible(true);
+    }
+}
