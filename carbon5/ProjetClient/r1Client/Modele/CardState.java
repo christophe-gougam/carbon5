cc86630969ef16c44bf3814db696120373059371
christophe.gougam <chris.g-93@hotmail.fr>
2017-03-14 11:17:32 +0100

    Ajout classes CardState et UrgencyDegree

---
+++ carbon5/ProjetClient/r1Client/Modele/CardState.java
@@ -0,0 +1,16 @@
+package r1Client.Modele;
+
+public class CardState {
+
+	private String description;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+}
