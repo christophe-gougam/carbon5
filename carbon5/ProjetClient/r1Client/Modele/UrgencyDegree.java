23577552ffd04aded4f5fdca207d9a45b12f3233
hung.phan <phuoc-hung.phan@etu.u-pec.fr>
2017-03-18 23:31:21 +0100

    DAO

--- carbon5/ProjetClient/r1Client/Modele/UrgencyDegree.java
+++ carbon5/ProjetClient/r1Client/Modele/UrgencyDegree.java
@@ -1,15 +1,31 @@
 package r1Client.Modele;
 
 public class UrgencyDegree {
-
+        private int id;
 	private String description;
+        
+        public UrgencyDegree(int id, String description){
+            
+        }
 
+        public UrgencyDegree() {
+            
+        }
+        
+        public int getId(){
+            return id;
+        }
+        
+        public void setId(int newId){
+            this.id = newId;
+        }
+        
 	public String getDescription() {
-		return description;
+            return description;
 	}
 
 	public void setDescription(String description) {
-		this.description = description;
+            this.description = description;
 	}
 	
 	
