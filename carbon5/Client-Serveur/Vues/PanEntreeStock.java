52e865746ca5ff809da365bfd34f0795091a0b23
hung.phan <phuoc-hung.phan@etu.u-pec.fr>
2017-05-15 15:30:32 +0200

    Modify IHM

--- carbon5/Client-Serveur/Vues/PanEntreeStock.java
+++ carbon5/Client-Serveur/Vues/PanEntreeStock.java
@@ -45,9 +45,7 @@ public class PanEntreeStock extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
         jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
         
         DefaultComboBoxModel model = new DefaultComboBoxModel();
         ArrayList<String> parts = new ArrayList<String>();
@@ -65,8 +63,6 @@ public class PanEntreeStock extends javax.swing.JPanel {
 
         jLabel4.setText("Quantite");
 
-        jLabel5.setText("Prix unitaire");
-
         jButton1.setText("Enregistrer");
         jButton1.addActionListener(new BoutonListener(this));
         jButton2.setText("Annuler");
@@ -82,13 +78,13 @@ public class PanEntreeStock extends javax.swing.JPanel {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
                             .addComponent(jLabel4)
-                            .addComponent(jLabel5))
+                        )
                         .addGap(18, 18, 18)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jComboBox1, 0, 248, Short.MAX_VALUE)
                             .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addComponent(jTextField4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 48, Short.MAX_VALUE)
-                                .addComponent(jTextField3, javax.swing.GroupLayout.Alignment.LEADING))))
+                                .addComponent(jTextField3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 48, Short.MAX_VALUE)
+                            )))
                     .addComponent(jLabel1)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton1)
@@ -109,10 +105,7 @@ public class PanEntreeStock extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
@@ -129,9 +122,7 @@ public class PanEntreeStock extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
     private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
     final static Logger logger = Logger.getLogger(Serveur.class);
     // End of variables declaration//GEN-END:variables
     /**
@@ -162,7 +153,7 @@ public class PanEntreeStock extends javax.swing.JPanel {
     		data.add(quantite);
     		data.add(""+User.getAllUsers().get(0).getId());
     		String identifier = "addEntryStock";
-    		logger.info("Entrée de stock");
+    		logger.info("Entrï¿½e de stock");
     		new ServerConnect(data, identifier, frame);
     	}
 	}
